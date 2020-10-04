7b0a8c281ef866edd50861b323178c8a6e7a0084
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-10-03 17:26:24 -0700

    Finish form functionality on front end

--- client/src/components/ExternalUsers.jsx
+++ client/src/components/ExternalUsers.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios';
 
 
 export default class ExternalUsers extends React.Component{
@@ -8,48 +9,97 @@ export default class ExternalUsers extends React.Component{
     this.state = {
       firstName: "",
       lastName: "",
-      telephoneNumber: "",
+      phoneNumber: "",
       fullAddress: "",
       ssn: ""
     };
-   
+   this.handleChange = this.handleChange.bind(this);
+   this.handleClick = this.handleClick.bind(this);
   }
 
-  
+  handleChange(event){
+   
+    const stateName = event.target.name;
+
+    this.setState({ [stateName]: event.target.value })
+  }
+
+  handleClick() {
+    axios.post('/data', {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      phoneNumber: this.state.phoneNumber,
+      fullAddress: this.state.fullAddress,
+      ssn: this.state.ssn
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+  }
 
   render(){
 
     return(
       <div>
        <form>
-       External users Frontend should provide a simple form for users to enter their personal
-info including First Name, Last Name, Telephone Number, Full Address, and SSN.
         <label htmlFor="firstName">
           First Name: 
-          <input name="firstName" type="text" id="firstName" value="First Name"/>
+          <input 
+          name="firstName" 
+          type="text" 
+          id="firstName" 
+          value={this.state.firstName}
+          onChange={this.handleChange}
+          />
         </label>
 
         <label htmlFor="lastName">
           Last Name:
-          <input name="lastName" type="text" id="lastName" value="Last Name"/>
+          <input name="lastName" 
+          type="text" 
+          id="lastName" 
+          value={this.state.lastName}
+          onChange={this.handleChange}
+          />
         </label>
 
         <label htmlFor="phone">
           Phone Number:
-          <input name="phone" type="tel" id="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" value="Phone Number"/>
+          <input name="phoneNumber" 
+          type="tel" 
+          id="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" 
+          value={this.state.phoneNumber}
+          onChange={this.handleChange}
+          />
         </label>
 
         <label htmlFor="fullAddress">
           Full Address:
-          <input name="fullAddress" type="text" id="fullAddress" value="Full Address"/>
+          <input name="fullAddress" 
+          type="text" 
+          id="fullAddress" 
+          value={this.state.fullAddress}
+          onChange={this.handleChange}
+          />
         </label>
 
         <label htmlFor="ssn">
           SSN: 
-          <input name="ssn" type="text" id="ssn" value="SSN"/>
+          <input 
+          name="ssn" 
+          type="text" 
+          id="ssn" 
+          pattern="[0-9]{3}-[0-9]{2}-[0-9]{4}"
+          value={this.state.ssn}
+          onChange={this.handleChange}
+          autoComplete="off"
+          />
         </label>
-
+        <button 
+        type="button" 
+        onClick={this.handleClick}>Submit</button>
        </form>
+       
       </div>
     )
   }
--- server/index.js
+++ server/index.js
@@ -10,7 +10,9 @@ let db = require('./controllers.js');
 app.use(express.static(path.join(__dirname, './../client/dist')));
 app.use(bodyParser.json());
 
-
+app.post("/data", (req, res) => {
+  console.log("Endpoint hit!")
+})
 
 
 let PORT = 3000;
